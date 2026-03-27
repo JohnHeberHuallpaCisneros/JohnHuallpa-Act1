f7e5aa5ddc32b343f4e1c802e51393b1aef744e2
John Heber Huallpa Cisneros <john.heber.huallpa.cisneros09@gmail.com>
2026-03-27 18:22:19 -0300

    ACT4

---
+++ JohnHuallpa-ACT4/4.2/Program4.2.cs
@@ -0,0 +1,28 @@
+﻿//2.Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc.
+//(No se ingresan valores por teclado)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2
+{
+    class P2
+    {
+        static void Main(String[] args)
+        {
+            int avance = 11;
+            int contador = 1;
+            while (contador <= 25)
+            {
+                if (contador < 25)
+                    Console.Write(avance + " - ");
+                else
+                    Console.Write(avance);
+                avance += 11;
+                contador++;
+            }
+        }
+    }
+}
---
+++ JohnHuallpa-ACT4/4.3/Program4.3.cs
@@ -0,0 +1,38 @@
+﻿/* 3.Realizar un programa que acumule (sume) valores ingresados por teclado hasta
+ingresar el 9999 (no sumar dicho valor, indica que ha finalizado la carga). Imprimir el
+valor acumulado e informar si dicho valor es cero, mayor a cero o menor a cero. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._3
+{
+    class P3
+    {
+        static void Main(String[] args)
+        {
+            int acumulador = 0;
+            int valor;
+            Console.Write("ingresa algun valor (con 9999 finaliza el programa): ");
+
+            while (true)
+            {
+                valor = int.Parse(Console.ReadLine());
+                if (valor == 9999)
+                {
+                    break;
+                }
+                acumulador += valor;
+            }
+            Console.WriteLine("valor acumulado= " + acumulador);
+            if (acumulador > 0)
+                Console.WriteLine("El valor es mayor a 0");
+            else if (acumulador < 0)
+                Console.WriteLine("El valor es menor a 0");
+            else
+                Console.WriteLine("el valor es 0");
+        }
+    }
+}
