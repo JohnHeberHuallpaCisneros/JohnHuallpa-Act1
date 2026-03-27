bb37db76f80dacb29923ba8adb1e55d3de27c477
John Heber Huallpa Cisneros <john.heber.huallpa.cisneros09@gmail.com>
2026-03-27 19:55:44 -0300

    actividad 5

---
+++ 5.1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 1.Confeccionar un programa que permita ingresar un valor del 1 al 10 y nos
+muestre la tabla de multiplicar del mismo (los primeros 13 términos)
+Ejemplo: Si ingreso 3 deberá aparecer en pantalla los valores 3, 6, 9, hasta el
+39. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+                Console.Write("Ingrese un número del 1 al 10: ");
+                num = int.Parse(Console.ReadLine());
+            } while (num < 1 || num > 10);
+
+            for (int i = 1; i <= 13; i++)
+            {
+                Console.WriteLine(num * i);
+            }
+        }
+    }
+}
---
+++ 5.3/Program.cs
@@ -0,0 +1,42 @@
+﻿/* 3. Se realiza la carga de 10 valores enteros por teclado. Se desea conocer:
+a) La cantidad de valores ingresados negativos.
+b) La cantidad de valores ingresados positivos.
+c) La cantidad de múltiplos de 15.
+d) El valor acumulado de los números ingresados que son pares. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int negativos = 0, positivos = 0, multiplos15 = 0, sumaPares = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("Ingrese un número: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 0)
+                    negativos++;
+                else if (num > 0)
+                    positivos++;
+
+                if (num % 15 == 0)
+                    multiplos15++;
+
+                if (num % 2 == 0)
+                    sumaPares += num;
+            }
+            Console.WriteLine("Negativos: " + negativos);
+            Console.WriteLine("Positivos: " + positivos);
+            Console.WriteLine("Múltiplos de 15: " + multiplos15);
+            Console.WriteLine("Suma de pares: " + sumaPares);
+        }
+    }
+}
