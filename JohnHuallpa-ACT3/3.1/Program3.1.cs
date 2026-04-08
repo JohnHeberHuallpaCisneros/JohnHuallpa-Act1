d72e8be6f3b2cd402cd663193a63aaac785af3b4
John Heber Huallpa Cisneros <john.heber.huallpa.cisneros09@gmail.com>
2026-04-07 22:35:50 -0300

    Add files via upload

---
+++ JohnHuallpa-ACT3/3.1/Program3.1.cs
@@ -0,0 +1,33 @@
+﻿/* Realizar un programa que pida cargar una fecha cualquiera,
+luego verificar si dicha fecha corresponde a Navidad.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Verifica si es Navidad ===\n");
+
+            Console.Write("Ingrese el día (1-31): ");
+            int dia = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el mes (1-12): ");
+            int mes = int.Parse(Console.ReadLine());
+
+            if (dia == 25 && mes == 12)
+            {
+                Console.WriteLine("\n¡Feliz Navidad!");
+            }
+            else
+            {
+                Console.WriteLine("\nLa fecha ingresada NO es la de Navidad.");
+            }
+        }
+    }
+}
