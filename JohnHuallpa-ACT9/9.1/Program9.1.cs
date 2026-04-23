045e436d96ce69aa03a69cacb738cc03803cb31d
John Heber Huallpa Cisneros <john.heber.huallpa.cisneros09@gmail.com>
2026-04-22 17:37:30 -0800

    Add files via upload

---
+++ JohnHuallpa-ACT9/9.1/Program9.1.cs
@@ -0,0 +1,95 @@
+﻿/*
+ Se desea desarrollar un programa que permita registrar los nombres y las
+calificaciones de 6 estudiantes. Luego de cargar los datos, se debe mostrar el
+nombre del estudiante con la nota más alta, junto con su nota. Al igual que el
+estudiante con la nota más baja. Informar si hay estudiantes con la misma nota
+máxima o mínima.
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1
+{
+    class Program
+    {
+        private string[] Alumnos;
+        private int[] Notas;
+        public void Carga()
+        {
+            Alumnos = new string[6];
+            Notas = new int[6];
+            for (int i = 0; i < Alumnos.Length; i++)
+            {
+                Console.WriteLine("Ingresa el nombre del alumno: ");
+                Alumnos[i] = Console.ReadLine();
+                Console.WriteLine("Ingrese la nota del alumno: ");
+                string notaingresada;
+                notaingresada = Console.ReadLine();
+                Notas[i] = int.Parse(notaingresada);
+            }
+        }
+        public void NA_NB()
+        {
+            string alumno_destacado = Alumnos[0];
+            int Nota_Alta = Notas[0];
+            string alumno_mediocre = Alumnos[0];
+            int Nota_Baja = Notas[0];
+            int rMax = 0;
+            int rMin = 0;
+            for (int i = 0; i < Alumnos.Length; i++)
+            {
+                if (Notas[i] > Nota_Alta)
+                {
+                    Nota_Alta = Notas[i];
+                    alumno_destacado = Alumnos[i];
+                }
+                if (Notas[i] < Nota_Baja)
+                {
+                    Nota_Baja = Notas[i];
+                    alumno_mediocre = Alumnos[i];
+                }
+            }
+            for (int i = 0; i < Alumnos.Length; i++)
+            {
+                if (Nota_Alta == Notas[i])
+                {
+                    rMax++;
+                }
+                if (Nota_Baja == Notas[i])
+                {
+                    rMin++;
+                }
+            }
+            Console.WriteLine("El alumno con la nota más alta es: " + alumno_destacado + (" con: ") + Nota_Alta);
+            Console.WriteLine("El alumno con la nota más baja es: " + alumno_mediocre + (" con: ") + Nota_Baja);
+            if (rMax > 1)
+            {
+                Console.WriteLine("La nota alta entre los alumnos se repite: " + rMax);
+            }
+            else
+            {
+                Console.WriteLine("La nota alta no se repite");
+            }
+            if (rMin > 1)
+            {
+                Console.WriteLine("La nota baja entre los alumnos se repite: " + rMin);
+            }
+            else
+            {
+                Console.WriteLine("La nota baja no se repite");
+            }
+            Console.ReadKey();
+        }
+     
+            static void Main(string[] args)
+            {
+                Program p = new Program();
+                p.Carga();
+                p.NA_NB();
+            }
+        
+}   }
---
+++ JohnHuallpa-ACT9/9.2/Program9.2.cs
@@ -0,0 +1,72 @@
+﻿/*
+Una empresa registra los nombres de sus 5 vendedores y el total de ventas
+realizadas por cada uno en un mes. Cargar los nombres y ventas en dos
+vectores paralelos, ordenar los datos de mayor a menor según las ventas,
+imprimir la lista ordenada con nombre y monto de la venta, e informar quien fue
+el que menos vendió de los 5 empleados.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._2
+{
+    class Program
+    {
+        private string[] Vendedores;
+        private int[] CVentas;
+        public void Carga()
+        {
+            Vendedores = new string[5];
+            CVentas = new int[5];
+            for (int i = 0; i < Vendedores.Length; i++)
+            {
+                Console.WriteLine("Ingresa un vendedor: ");
+                Vendedores[i] = Console.ReadLine();
+                Console.WriteLine("Ingresa su total de ventas realizadas: ");
+                string valor;
+                valor = Console.ReadLine();
+                CVentas[i] = int.Parse(valor);
+            }
+        }
+        public void Mostrar_Ordenar()
+        {
+            for (int i = 0; i < Vendedores.Length - 1; i++)
+            {
+                for (int j = 0; j < Vendedores.Length - 1 - i; j++)
+                {
+                    if (CVentas[j] < CVentas[j + 1])
+                    {
+                        int aux = CVentas[j];
+                        CVentas[j] = CVentas[j + 1];
+                        CVentas[j + 1] = aux;
+
+                        string aux2 = Vendedores[j];
+                        Vendedores[j] = Vendedores[j + 1];
+                        Vendedores[j + 1] = aux2;
+                    }
+                }
+            }
+            int menos_ventas = CVentas[CVentas.Length - 1];
+            string v_menos = Vendedores[Vendedores.Length - 1];
+
+            Console.WriteLine("Vendedores ordenados en cantidad de ventas (mayor a menor): ");
+                for (int i = 0; i < Vendedores.Length; i++)
+            {
+                Console.Write(Vendedores[i] + " con el monto de venta de: " + CVentas[i]);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Vendedor que vendio menos: " + v_menos + " con la cantidad de: " + menos_ventas);
+
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Carga();
+            p.Mostrar_Ordenar();
+        }
+    }
+}
